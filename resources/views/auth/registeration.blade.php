3448c8d17ff765403a00adb8ffd5be897a2c853c
hfasad <hfasad0@gmail.com>
2023-12-05 02:03:05 -0800

    changes

--- resources/views/auth/registeration.blade.php
+++ resources/views/auth/registeration.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Banking App</title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="css/style1.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
